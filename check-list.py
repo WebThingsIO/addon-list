9a8866b6ada9bc3876554997e080b87d6c3bd7d0
Mike Stegeman <mrstegeman@gmail.com>
2018-02-09 17:44:12 +0000

    Add travis-ci script to verify list. (#12)

---
+++ check-list.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+
+import hashlib
+import json
+import os
+import re
+import shutil
+import sys
+import tarfile
+import urllib.error
+import urllib.request
+
+
+_LIST = './list.json'
+
+
+def cleanup(exit=True):
+    if os.path.exists('package.tgz'):
+        os.unlink('package.tgz')
+
+    if os.path.exists('package'):
+        shutil.rmtree('package')
+
+    if exit:
+        sys.exit(1)
+
+
+def verify_fields(fields, obj):
+    for field in fields:
+        if '.' in field:
+            _fields = field.split('.')
+        else:
+            _fields = [field]
+
+        e = obj
+        for f in _fields:
+            if f not in e:
+                return False, field
+
+            e = e[f]
+
+    return True, None
+
+
+def hash_file(fname):
+    hasher = hashlib.sha256()
+    try:
+        with open(fname, 'rb') as f:
+            while True:
+                buf = f.read(4096)
+                if len(buf) == 0:
+                    break
+
+                hasher.update(buf)
+
+        return hasher.hexdigest()
+    except (IOError, OSError):
+        return None
+
+
+def main():
+    # Make sure the file is valid JSON
+    try:
+        with open(_LIST, 'rt') as f:
+            addon_list = json.load(f)
+    except (IOError, OSError, ValueError):
+        print('Failed to read list file.')
+        sys.exit(1)
+
+    required_in_list = [
+        'name',
+        'display_name',
+        'description',
+        'version',
+        'url',
+        'api.min',
+        'api.max',
+    ]
+
+    required_in_manifest = [
+        'name',
+        'version',
+        'files',
+        'moziot.api.min',
+        'moziot.api.max',
+    ]
+
+    for entry in addon_list:
+        success, field = verify_fields(required_in_list, entry)
+        if not success:
+            print('Field "{}" missing from:\n{}'
+                  .format(field, json.dumps(entry, indent=2)))
+            sys.exit(1)
+
+        name = entry['name']
+
+        # Download the package.
+        try:
+            urllib.request.urlretrieve(entry['url'], 'package.tgz')
+        except urllib.error.URLError:
+            print('Failed to download package for "{}"'.format(name))
+            cleanup()
+
+        # Check the package contents.
+        try:
+            with tarfile.open('./package.tgz', 'r:gz') as t:
+                t.extractall()
+        except (IOError, OSError, tarfile.TarError):
+            print('Failed to untar package for "{}"'.format(name))
+            cleanup()
+
+        try:
+            with open('./package/package.json', 'rt') as f:
+                manifest = json.load(f)
+        except (IOError, OSError, ValueError):
+            print('Failed to read package.json for "{}"'.format(name))
+            cleanup()
+
+        # Verify required fields in package.json.
+        success, field = verify_fields(required_in_manifest, manifest)
+        if not success:
+            print('Field "{}" missing from:\n{}'
+                  .format(field, json.dumps(manifest, indent=2)))
+            cleanup()
+
+        # Verify some additional fields.
+        if not isinstance(manifest['files'], list) or \
+                len(manifest['files']) == 0:
+            print('Invalid files array:\n{}'
+                  .format(json.dumps(manifest, indent=2)))
+            cleanup()
+
+        if 'config' in manifest and not isinstance(manifest['config'], dict):
+            print('Invalid config object:\n{}'
+                  .format(json.dumps(manifest, indent=2)))
+            cleanup()
+
+        # Verify the files list.
+        for fname in manifest['files']:
+            if not os.path.exists(os.path.join('package', fname)):
+                print('File missing for package "{}": {}'.format(name, fname))
+                cleanup()
+
+        # Verify SHA256SUMS, if present.
+        if 'SHA256SUMS' in manifest['files']:
+            try:
+                with open('./package/SHA256SUMS', 'rt') as f:
+                    for line in f:
+                        cksum, fname = \
+                            re.split(r'\s+', line.strip(), maxsplit=1)
+                        fname = os.path.join('package', fname)
+                        if cksum != hash_file(fname):
+                            print('Checksum failed in package "{}": {}'
+                                  .format(name, fname))
+                            cleanup()
+            except (IOError, OSError, ValueError):
+                print('Failed to read SHA256SUMS file for package "{}"'
+                      .format(name))
+                cleanup()
+
+        cleanup(exit=False)
+
+
+if __name__ == '__main__':
+    main()
