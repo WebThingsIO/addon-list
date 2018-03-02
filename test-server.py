d3cd8e864a0a61d70ffe7d1b891a8b610bcef47b
Mike Stegeman <mrstegeman@gmail.com>
2018-03-02 08:54:01 -0700

    Update most add-ons to support API level 2.
    Also, add a test server for local testing.

---
+++ test-server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+
+if __name__ == '__main__':
+    addr = ('127.0.0.1', 8088)
+    httpd = HTTPServer(addr, CORSRequestHandler)
+
+    print('Change your gateway config as such:\n'
+          '\n'
+          '  addonManager: {\n'
+          '    ...\n'
+          '    listUrl: \'http://127.0.0.1:8088/list.json\',\n'
+          '    ...\n'
+          '  },')
+    httpd.serve_forever()
