8302432246bf43b9a0e3f5dbe471b34610bc16f6
Michael Stegeman <michael@stegeman.me>
2019-09-11 12:05:55 -0800

    Verify manifest.json files.

--- tools/check-list.py
+++ tools/check-list.py
@@ -131,8 +131,7 @@ def verify_package_json(package_json, list_entry, package):
         print('Homepage mismatch for package "{}": '
               'homepage from package.json "{}" doesn\'t match '
               'homepage from list.json "{}"'
-              .format(name, package_json['homepage'],
-                      list_entry['homepage']))
+              .format(name, package_json['homepage'], list_entry['homepage']))
         cleanup()
 
     # Verify that the type matches
@@ -176,7 +175,151 @@ def verify_package_json(package_json, list_entry, package):
 
 
 def verify_manifest_json(manifest_json, list_entry, package):
-    pass
+    name = list_entry['name']
+    webthings = manifest_json['gateway_specific_settings']['webthings']
+
+    if 'enabled' in webthings and \
+            webthings['enabled'] and \
+            list_entry['author'].lower() != 'mozilla iot':
+        print('Add-on is enabled by default: {}'.format(name))
+        cleanup()
+
+    # Verify SHA256SUMS.
+    if not os.path.exists('./package/SHA256SUMS'):
+        print('SHA256SUMS file is missing from package "{}"'.format(name))
+        cleanup()
+
+    sums_file = os.path.realpath('./package/SHA256SUMS')
+    sums = {}
+    try:
+        with open(sums_file, 'rt') as f:
+            for line in f:
+                cksum, fname = re.split(r'\s+', line.strip(), maxsplit=1)
+                fname = os.path.realpath(os.path.join('package', fname))
+
+                if not os.path.exists(fname):
+                    print('File "{}" missing for add-on "{}"'
+                          .format(fname, name))
+                    cleanup()
+
+                sums[fname] = cksum
+    except (IOError, OSError, ValueError):
+        print('Failed to read SHA256SUMS file for package "{}"'.format(name))
+        cleanup()
+
+    files = []
+    for (dpath, _, fnames) in os.walk('package'):
+        files.extend(
+            [os.path.realpath(os.path.join(dpath, fname)) for fname in fnames]
+        )
+
+    for fname in files:
+        if fname == sums_file:
+            continue
+
+        if fname not in sums:
+            print('Checksum missing for file "{}" in package "{}"'
+                  .format(fname, name))
+            cleanup()
+
+        if sums[fname] != hash_file(fname):
+            print('Checksum failed in package "{}": {}'.format(name, fname))
+            cleanup()
+
+    # Verify that the name matches
+    if manifest_json['id'] != name:
+        print('ID mismatch for package "{}"'
+              'ID from manifest.json "{}" doesn\'t match '
+              'name from list.json'
+              .format(name, manifest_json['id']))
+        cleanup()
+
+    # Verify that the version matches
+    if manifest_json['version'] != package['version']:
+        print('Version mismatch for package "{}": '
+              'version from manifest.json "{}" doesn\'t match '
+              'version from list.json "{}"'
+              .format(name, manifest_json['version'], package['version']))
+        cleanup()
+
+    # Verify that the author matches
+    if manifest_json['author'].split('<')[0].strip() != list_entry['author']:
+        print('Author mismatch for package "{}": '
+              'author from manifest.json "{}" doesn\'t match '
+              'author from list.json "{}"'
+              .format(name, manifest_json['author'], list_entry['author']))
+        cleanup()
+
+    # Verify that the display name matches
+    if manifest_json['name'] != list_entry['display_name']:
+        print('Name mismatch for package "{}": '
+              'name from manifest.json "{}" doesn\'t '
+              'match display_name from list.json "{}"'
+              .format(name, manifest_json['name'], list_entry['display_name']))
+        cleanup()
+
+    # Verify that the homepage matches
+    if manifest_json['homepage_url'] != list_entry['homepage']:
+        print('Homepage mismatch for package "{}": '
+              'homepage_url from manifest.json "{}" doesn\'t match '
+              'homepage from list.json "{}"'
+              .format(name, manifest_json['homepage_url'],
+                      list_entry['homepage']))
+        cleanup()
+
+    # Verify that the type matches
+    t = webthings['primary_type']
+    if t != list_entry['type']:
+        print('type mismatch for package "{}": '
+              'primary_type from manifest.json "{}" doesn\'t match type '
+              'from list.json "{}"'
+              .format(name, t, list_entry['type']))
+        cleanup()
+
+    gw_min = '0.10.0'
+    if 'strict_min_version' in webthings:
+        gw_min = webthings['strict_min_version']
+
+    if package['gateway']['min'] != gw_min:
+        print('Minimum gateway version mismatch for package "{}": '
+              'strict_min_version from manifest.json "{}" doesn\'t match '
+              'min from list.json "{}"'
+              .format(name, gw_min, package['gateway']['min']))
+        cleanup()
+
+    gw_max = '*'
+    if 'strict_max_version' in webthings:
+        gw_max = webthings['strict_max_version']
+
+    if package['gateway']['max'] != gw_max:
+        print('Maximum gateway version mismatch for package "{}": '
+              'strict_max_version from manifest.json "{}" doesn\'t match '
+              'max from list.json "{}"'
+              .format(name, gw_max, package['gateway']['max']))
+        cleanup()
+
+    # Validate the config schema, if present
+    if 'options' in manifest_json and 'schema' in manifest_json['options']:
+        try:
+            jsonschema.Draft7Validator.check_schema(
+                manifest_json['options']['schema'])
+        except jsonschema.SchemaError as e:
+            print('Invalid config schema for {}: {}'
+                  .format(name, e))
+            cleanup()
+
+    if 'default_locale' in manifest_json:
+        dname = os.path.join(
+            '.',
+            'package',
+            '_locales',
+            manifest_json['default_locale']
+        )
+
+        if not os.path.exists(dname):
+            print('Default locale "{}" does not exist in package "{}"'
+                  .format(manifest_json['default_locale'], name))
+            cleanup()
 
 
 def main():
@@ -191,13 +334,13 @@ def main():
     with open(_PACKAGE_SCHEMA) as f:
         package_schema = json.load(f)
 
-#    with open(_MANIFEST_SCHEMA) as f:
-#        manifest_schema = json.load(f)
+    with open(_MANIFEST_SCHEMA) as f:
+        manifest_schema = json.load(f)
 
     try:
         jsonschema.Draft7Validator.check_schema(list_schema)
         jsonschema.Draft7Validator.check_schema(package_schema)
-#        jsonschema.Draft7Validator.check_schema(manifest_schema)
+        jsonschema.Draft7Validator.check_schema(manifest_schema)
     except jsonschema.SchemaError as e:
         print('Schema validation failed: {}'.format(e))
         cleanup()
@@ -285,12 +428,18 @@ def main():
                 print('Failed to read package.json for "{}"'.format(name))
                 cleanup()
 
-#            try:
-#                with open('./package/manifest.json', 'rt') as f:
-#                    manifest_json = json.load(f)
-#            except (IOError, OSError, ValueError):
-#                print('Failed to read manifest.json for "{}"'.format(name))
-#                cleanup()
+            manifest_json = None
+            if os.path.exists('./package/manifest.json'):
+                try:
+                    with open('./package/manifest.json', 'rt') as f:
+                        manifest_json = json.load(f)
+                except (IOError, OSError, ValueError):
+                    print('Failed to read manifest.json for "{}"'.format(name))
+                    cleanup()
+            else:
+                print('manifest.json is missing for "{}"'.format(name))
+                # TODO: enforce this once all add-ons have transitioned
+                # cleanup()
 
             licenses = glob.glob('./package/LICENSE*')
             if len(licenses) == 0:
@@ -302,8 +451,9 @@ def main():
             verify_package_json(package_json, entry, package)
 
             # Verify required fields in manifest.json.
-#            jsonschema.validate(manifest_json, manifest_schema)
-#            verify_manifest_json(manifest_json, entry)
+            if manifest_json is not None:
+                jsonschema.validate(manifest_json, manifest_schema)
+                verify_manifest_json(manifest_json, entry, package)
 
             cleanup(exit=False)
 
